d179138e320825ac8ea79556afb5c154e5c3cd49
AdeMuhammadFarid <gakutonijiro@gmail.com>
2025-01-13 17:22:04 +0700

    Remove unused PNG from drawable

--- app/src/main/java/com/example/movieku/Splash/Splash.java
+++ app/src/main/java/com/example/movieku/Splash/Splash.java
@@ -1,33 +1,67 @@
 package com.example.movieku.Splash;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ActivityOptions;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
+import android.util.Pair;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.movieku.Login.LoginActivity;
-import com.example.movieku.R;
 
-public class Splash extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
+    private static final int SPLASH_SCREEN_DURATION = 3000; // Durasi splash screen dalam milidetik
 
+    // Deklarasi untuk animasi dan elemen-elemen UI
+    private Animation topAnim, bottomAnim;
+    private ImageView img;
+    private TextView txtNameApp, txtSubNameApp, txtCopyright;
+
+    @RequiresApi(api = Build.VERSION_CODES.P) // Memastikan aplikasi hanya berjalan di perangkat dengan Android P atau lebih baru
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash);
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+        // Mengatur tampilan aplikasi menjadi fullscreen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main); // Menghubungkan layout dengan activity
 
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().hide();
-        }
+        // Memuat animasi dari resource animasi
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        // Menghubungkan elemen UI dengan ID yang sesuai
+        img = findViewById(R.id.img_logo);
+        txtNameApp = findViewById(R.id.tv_nameApp);
+        txtSubNameApp = findViewById(R.id.tv_sub_nameApp);
+        txtCopyright = findViewById(R.id.tv_copyright);
+
+        // Menambahkan animasi pada elemen-elemen UI
+        img.setAnimation(topAnim);
+        txtNameApp.setAnimation(bottomAnim);
+        txtSubNameApp.setAnimation(bottomAnim);
+        txtCopyright.setAnimation(bottomAnim);
+
+        // Menggunakan Handler untuk menunda eksekusi selama SPLASH_SCREEN (3000ms atau 3 detik)
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(Splash.this, LoginActivity.class));
-                finish();
+                // Setelah durasi selesai, mengarahkan pengguna ke halaman login
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+
+                // Menambahkan transisi animasi saat berpindah ke LoginActivity
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,
+                        Pair.create(img, "logo_img"),
+                        Pair.create(txtNameApp, "textnameApp"));
+                startActivity(intent, options.toBundle());
+                finish(); // Menutup activity utama agar tidak bisa kembali ke halaman splash screen
             }
-        }, 3000);
+        }, SPLASH_SCREEN_DURATION); // Menunggu selama 3000ms (3 detik)
     }
 }
